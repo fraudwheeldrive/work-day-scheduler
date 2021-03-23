4695b749239fa1c1f9c30153713a99ad9eca1208
[Andrew Core] <[adcore89@gmail.com]>
2021-03-23 07:34:43 -0400

    pseudo code

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -8,6 +8,21 @@ var now = moment().format ("H A");
 
 $("#currentDay").text(today);
 
+//WHEN I scroll down
+//THEN I am presented with time blocks for standard business hours
+//WHEN I view the time blocks for that day
+
+//THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+//WHEN I click into a time block
+//THEN I can enter an event
+
+//WHEN I click the save button for that time block
+//THEN the text for that event is saved in local storage
+
+//WHEN I refresh the page
+//THEN the saved events persist
 
 
 
+.
